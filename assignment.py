5c4f57af7ec0a6dd984207d1f280c262dc46b675
d-aravind <daravind396@gmail.com>
2015-03-08 16:21:05 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -498,6 +498,7 @@ for method in ["newton","lagrange"]:
 
 # New Class #
 
+import math
 from math import *
 class Integrate:
 
@@ -505,22 +506,28 @@ class Integrate:
     # Name  : D Aravind
     # S.no. : 24
 
+    def __init__(self):
+        self.function=None
+        self.interval=None
+        self.no_of_partitions=None
+        self.result=None
+
     def solve(self,S,interval,N,method):
         if(method=="trapezoid"):
             return (self.TrapezoidalRule(S,interval,N))
         else:
             return (self.SimpsonsRule(S,interval,N))
     
-    def TrapezoidalRule(self,S,interval,N):                             # The function takes 3 user inputs
+    def TrapezoidalRule(self,S,interval,N):                                 # The function takes 3 user inputs
         
         # S is the string containing the function to be
         # integrated. The string must contain only those 
         # functions which are available in "math" module,
         # and the syntax used in S must be the same as is
         # used in Python. e.g.,
-        # If f(x)=sin(x)^2 + cos(x), then --
-        # S=sin(x)**2+cos(x) OR
-        # S=pow(sin(x),2)+cos(x)
+        # If f(x)=sin(x)^2 + cos(x) + e^(x^0.5) + log(x), then --
+        # S=sin(x)**2+cos(x)+exp(x**0.5)+log(x) OR
+        # S=pow(sin(x),2)+cos(x)+exp(pow(x,0.5))+log(x)
         # L is a list of two values containing the lower
         # and upper limits of integration. e.g.,-
         # If we have to integrate f(x) from 1.5 to 6, then--
@@ -528,16 +535,22 @@ class Integrate:
         # N is the variable used for controlling the number
         # of partitions.
         
-        fun=lambda x:eval(S)                                            # lambda function which converts user input into a function
-        length=(interval[1]-interval[0])/N                              # variable to store the length of each sub-interval
-        sum_of_values=fun(interval[0])                                  # variable to store sum of function values
-        for i in range(1,N):                                            # calculating sum of function values
-            sum_of_values+=2*fun(interval[0]+length*i)
-        sum_of_values+=fun(interval[1])
-        result=((interval[1]-interval[0])*sum_of_values)/(2*N)          # calculating result
-        return (result)                                                 # return result
+        self.interval=interval
+        self.no_of_partitions=N
+        try:
+            (lambda x:eval(S))(self.interval[0])
+            self.function=lambda x:eval(S)                                  # lambda function which converts user input into a function
+        except:
+            return("Input is not in proper Python syntax")
+        length=(self.interval[1]-self.interval[0])/self.no_of_partitions    # variable to store the length of each sub-interval
+        sum_of_values=self.function(self.interval[0])                       # variable to store sum of function values
+        for i in range(1,N):                                                # calculating sum of function values
+            sum_of_values+=2*self.function(self.interval[0]+length*i)
+        sum_of_values+=self.function(self.interval[1])
+        self.result=((self.interval[1]-self.interval[0])*sum_of_values)/(2*self.no_of_partitions)  # calculating result
+        return (self.result)                                                # return result
     
-    def SimpsonsRule(self,S,interval,N):                                # The function takes 3 user inputs
+    def SimpsonsRule(self,S,interval,N):                                    # The function takes 3 user inputs
 
         # S is the string containing the function to be
         # integrated. The string must contain only those 
@@ -545,8 +558,8 @@ class Integrate:
         # and the syntax used in S must be the same as is
         # used in Python. e.g.,
         # If f(x)=sin(x)^2 + cos(x), then --
-        # S=sin(x)**2+cos(x) OR
-        # S=pow(sin(x),2)+cos(x)
+        # S=sin(x)**2+cos(x)+exp(x**0.5)+log(x) OR
+        # S=pow(sin(x),2)+cos(x)+exp(pow(x,0.5))+log(x)
         # L is a list of two values containing the lower
         # and upper limits of integration. e.g.,-
         # If we have to integrate f(x) from 1.5 to 6, then--
@@ -554,17 +567,23 @@ class Integrate:
         # N is the variable used for controlling the number
         # of partitions.
 
-        fun=lambda x:eval(S)                                        # lambda function which converts user input into a function
-        length=(interval[1]-interval[0])/(2*N)                      # variable to store the length of each sub-interval
-        sum_of_values=fun(interval[0])                              # variable to store sum of function values
-        for i in range(1,2*N):                                      # calculating sum of function values
-            temp=fun(interval[0]+length*i)
+        self.interval=interval
+        self.no_of_partitions=N
+        try:
+            (lambda x:eval(S))(self.interval[0])
+            self.function=lambda x:eval(S)                                  # lambda function which converts user input into a function
+        except:
+            return("Input is not in proper Python syntax")
+        length=(self.interval[1]-self.interval[0])/(2*self.no_of_partitions)    # variable to store the length of each sub-interval
+        sum_of_values=self.function(self.interval[0])                       # variable to store sum of function values
+        for i in range(1,2*N):                                              # calculating sum of function values
+            temp=self.function(self.interval[0]+length*i)
             sum_of_values+=2*temp if i%2==0 else 4*temp
-        sum_of_values+=fun(interval[1])
-        result=((interval[1]-interval[0])*sum_of_values)/(6*N)      # calculating result
-        return (result)                                             # return result      
+        sum_of_values+=self.function(self.interval[1])
+        self.result=((self.interval[1]-self.interval[0])*sum_of_values)/(6*self.no_of_partitions)      # calculating result
+        return (self.result)                                                # return result      
     
 igr=Integrate()                                                     # object creation
 for method in ["trapezoid","simpson"]:
-    solution=igr.solve("pow(sin(x),2)+cos(x)+x",[0,1.57],10000,method)
+    solution=igr.solve("pow(sin(x),2)+cos(x)+exp(x**0.5)+log(x)",[1,2],10000,method)
     print (solution)
